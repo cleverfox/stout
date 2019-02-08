63eee0748d1ca1ebc2c2ec71261c83a98d9c0fcc
Andrew Beyond <spam@lykorian.org.ru>
2019-02-08 10:55:44 +0300

    verbose on headers generation

--- src/stout_genhdr.erl
+++ src/stout_genhdr.erl
@@ -2,35 +2,41 @@
 -export([main/1]).
 
 main(_) ->
-  lognames().
+  io:format("generating stout headers...~n"),
+  lognames(),
+  io:format("done.~n").
 
 lognames() ->
   Root = os:getenv("REBAR_ROOT_DIR", "."),
   
   %filelib:wildcard(Root ++ "/apps/**/*.{erl,hrl}").
-  SearchPaths=filelib:wildcard(Root ++ "/apps/**/*.stout")
-  ++ filelib:wildcard(Root ++ "/src/**/*.stout"),
-  Messages=lists:foldl(
+  SearchPaths =
+    filelib:wildcard(Root ++ "/apps/**/*.stout")
+    ++ filelib:wildcard(Root ++ "/src/**/*.stout"),
+  
+  Messages = lists:foldl(
     fun(Filename, Acc) ->
-        {ok, F} = file:consult(Filename),
-        lists:foldl(
-          fun({K,Fmt}, A) ->
-              [{K,Fmt,[]}|A];
-             ({K,Fmt,Opts}, A) ->
-              [{K,Fmt,Opts}|A]
-          end, Acc, F)
+      {ok, F} = file:consult(Filename),
+      lists:foldl(
+        fun({K, Fmt}, A) ->
+          [{K, Fmt, []} | A];
+          ({K, Fmt, Opts}, A) ->
+            [{K, Fmt, Opts} | A]
+        end, Acc, F)
     end, [], SearchPaths
-   ),
-  MsgNames=[ N || {N,_,_} <- Messages ],
-  MsgFmt=[ {N,F} || {N,F,_} <- Messages ],
-  MsgOpt=[ {N,O} || {N,_,O} <- Messages ],
+  ),
+  
+  MsgNames = [N || {N, _, _} <- Messages],
+  MsgFmt = [{N, F} || {N, F, _} <- Messages],
+  MsgOpt = [{N, O} || {N, _, O} <- Messages],
+  
   ok = filelib:ensure_dir(Root ++ "/include/"),
-  file:write_file(Root ++ "/include/stout_names.hrl",
-                  [
-                  io_lib:format("-define(STOUT_NAMES,~p).~n", [MsgNames]),
-                  io_lib:format("-define(STOUT_FORMATS,~p).~n", [maps:from_list(MsgFmt)]),
-                  io_lib:format("-define(STOUT_OPTS,~p).~n", [maps:from_list(MsgOpt)])
-                  ]
-                 ).
-
-
+  
+  file:write_file(
+    Root ++ "/include/stout_names.hrl",
+    [
+      io_lib:format("-define(STOUT_NAMES,~p).~n", [MsgNames]),
+      io_lib:format("-define(STOUT_FORMATS,~p).~n", [maps:from_list(MsgFmt)]),
+      io_lib:format("-define(STOUT_OPTS,~p).~n", [maps:from_list(MsgOpt)])
+    ]
+  ).
